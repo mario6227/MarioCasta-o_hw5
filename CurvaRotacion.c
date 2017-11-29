2685b4038c51931055168faf6b094337e839ceb0
Mario C <ma.castano11@uniandes.edu.co>
2017-11-29 10:08:46 -0500

    Distrib. normal

--- CurvaRotacion.c
+++ CurvaRotacion.c
@@ -2,8 +2,9 @@
 #include <math.h>
 #include <stdlib.h>
 #define n 300 //Cantidad de datos observados
+#define PI 3.14159265359
 
-//Definicion de funciones, f_modelo y de 'likelihood'
+//Definicion de funciones, f_modelo, randnormal y de 'likelihood'
 double likelihood(double obs[n], double model[n]){
   double suma = 0;
   int i;
@@ -11,6 +12,15 @@ double likelihood(double obs[n], double model[n]){
     suma = suma + pow((obs[i]-model[i]),2);
   }
   return exp(-(1.0/2.0)*suma);
+}
+
+//Distribucion normal usando el metodo de Box-Muller
+double randnormal(double mu, double s){
+  double u = drand48();
+  double v = drand48();
+  double Z = sqrt(-2.0*log(u))*cos(2*PI*v);
+
+  return (Z*s + mu);
 }
 
 double * v_modelo(double R[n], double Mb, double Md, double Mh){
@@ -43,6 +53,7 @@ int main(void){
   double * Mh_walk = malloc(n*sizeof(double));
   double * l_walk = malloc(n*sizeof(double));
   double * v_init = malloc(n*sizeof(double));
+  double * v_prime = malloc(n*sizeof(double));
   
   FILE * data; //Archivo de velocidades observadas
   data = fopen("RadialVelocities.dat","r");
@@ -60,6 +71,10 @@ int main(void){
 
   v_init = v_modelo(r_obs, Mb_walk[0], Md_walk[0], Mh_walk[0]);
   l_walk[0] = likelihood(v_obs, v_init);
+
+  int iter = 20000; //Numero de iteraciones a realizar
+  for(i=0; i<iter; i++){
+  }
     
   return 0;
 }
